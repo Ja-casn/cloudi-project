442fdd37ddfdd87b43cc8c2aadeb1b8610e8bf96
Jcasanova <jacapersonal04@gmail.com>
2023-03-07 01:31:03 +0100

    fix resolutions of final step image

--- src/pages/index.js
+++ src/pages/index.js
@@ -84,7 +84,7 @@ export default function Home() {
         <meta name="description" content="Cloudinary Hackaton Project" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" href="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20100%20100%22%3E%3Ctext%20y%3D%22.9em%22%20font-size%3D%2290%22%3E%F0%9F%8C%84%3C%2Ftext%3E%3C%2Fsvg%3E" type="image/svg+xml" />
-        
+
       </Head>
       <main className={styles.main}>
 
@@ -165,9 +165,8 @@ export default function Home() {
             {/* BACKGROUND IMAGES */}
             <CldImage
               src={background}
-              width='320'
-              height='320'
-              // crop='fill'
+              width='800'
+              height='600'
               grayscale={grayscal}
               blur={blured}
               pixelate={pixelated}
