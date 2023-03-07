97224534af559c925bb3ca1abeb5431118cb002e
Jcasanova <jacapersonal04@gmail.com>
2023-03-07 01:25:15 +0100

    editing generic favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,7 +83,8 @@ export default function Home() {
         <title>Backgrounds sicks</title>
         <meta name="description" content="Cloudinary Hackaton Project" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20100%20100%22%3E%3Ctext%20y%3D%22.9em%22%20font-size%3D%2290%22%3E%F0%9F%8C%84%3C%2Ftext%3E%3C%2Fsvg%3E" type="image/svg+xml" />
+        
       </Head>
       <main className={styles.main}>
 
